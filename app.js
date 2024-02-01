412c9976cde9ed559fcfad800d5c85bdace8d0d0
Naronglit2015 <nongmnakoy1123@gmail.com>
2024-02-01 14:44:28 +0700

    express prisma : register ok

--- app.js
+++ app.js
@@ -1,1 +1,19 @@
 require('dotenv').config()
+const express = require('express')
+const cors =require('cors')
+const notFound =require('./middlewares/notfound')
+const errorMiddleware = require('./middlewares/error')
+const authRoute = require('./routes/auth-route')
+const app = express()
+
+app.use(cors())
+app.use(express.json())
+
+app.use('/auth',authRoute)
+
+app.use(notFound)
+
+app.use(errorMiddleware)
+
+let port= process.env.PORT || 8000
+app.listen (port,()=> console.log('Server on Port :' ,port))
